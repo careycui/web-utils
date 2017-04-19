1be86b1ebf44b741e976bf4c0b1d138ebb30c40a
careycui <haiyucui@gmail.com>
2017-04-19 21:51:03 +0800

    init page slide

--- page_slide/src/ys-slide.js
+++ page_slide/src/ys-slide.js
@@ -21,11 +21,32 @@
 	}
 })(jQuery,function($){
 	"use strict";
-
-	
-	function Slide(){
-
+	var flag = false,
+		defaults = {
+			sectionPanel: '.panel', //代表每屏
+			scrollSpeed: 600, //滚动速度
+			nav: false, //是否显示导航
+			scrollbar: false, //是否显示滚动条
+			keyAble: false //键盘方向键是否可以控制滚动
+		};
+	/**
+	 * 全屏滚动构造方法
+	 * @param {object} me 全屏滑动容器
+	 * @param {object} options 插件配置
+	 */
+	function PageSlide(me, options){
+		this.me = me;
+		this.options = $.extend({}, options, defaults);
 	}
+	/**
+	 * 插件初始化
+	 * @return null
+	 */
+	PageSlide.prototype.init = function (){
+		var that = this,
+			ops = that.options,
+			heights = [];//每个panel高度，滚动距离
+	};
 	$.extend({
 		alert: function(){
 			console.log('test test test');
