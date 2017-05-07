b4a5c74b3095794eb4e3d6a68004b0d3630f75d5
careycui <haiyucui@gmail.com>
2017-05-08 01:09:20 +0800

    complete docs view type

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,30 @@
 
 const gulp = require('gulp');
 const pump = require('pump');
+const markdown = require('gulp-markdown');
+const rename = require('gulp-rename');
 
-gulp.task('copy',function(cb){
+gulp.task('copy', function(cb){
 	pump([
-			gulp.src(['**/dist/*+(.js|.css)','!(node_modules|docs|lib)/**/dist/*+(.js|.css)','!**/node_modules/**/dist/*+(.js|.css)']),
+			gulp.src(['**/dist/*+(.js|.css)','!node_modules/**/dist/*+(.js|.css)','!lib/**/dist/*+(.js|.css)','!docs/**/dist/*+(.js|.css)','!**/node_modules/**/dist/*+(.js|.css)']),
 			gulp.dest('docs/demo/')
 		],cb);
 });
+
+gulp.task('markdown', function(cb){
+	pump([
+			gulp.src(['**/*.md','!node_modules/**/*.md','!docs/**/*.md','!lib/**/*.md','!**/node_modules/**/*.md']),
+			markdown({
+				highlight: function (code, lang, callback) {
+				    require('pygmentize-bundled')({ lang: lang, format: 'html' }, code, function (err, result) {
+				      callback(err, result.toString());
+				    });
+				  }
+			}),
+			rename({
+				basename: 'api'
+			}),
+			gulp.dest('docs/demo/')
+		],cb);
+});
+gulp.task('release',['copy', 'markdown']);
