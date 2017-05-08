539a8401f113481ae888d42ad87656c6af47733e
careycui <cuihaiyu@hikvision.com>
2017-05-08 14:41:59 +0800

    complete docs

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,18 @@ const gulp = require('gulp');
 const pump = require('pump');
 const markdown = require('gulp-markdown');
 const rename = require('gulp-rename');
+const inject = require('gulp-inject-string');
+
+const startH = '<!DOCTYPE html>'+
+					'<html>'+
+						'<head>'+
+							'<meta charset="utf-8">'+
+							'<link rel="stylesheet" type="text/css" href="../../asset/sys/sys.min.css">'+
+							'<link rel="stylesheet" type="text/css" href="../../asset/markdown/markdown.css">'+
+						'</head>'+
+						'<body class="markdown-body">';
+
+const endH = '</body></html>';
 
 gulp.task('copy', function(cb){
 	pump([
@@ -22,6 +34,7 @@ gulp.task('markdown', function(cb){
 				    });
 				  }
 			}),
+			inject.wrap(startH,endH),
 			rename({
 				basename: 'api'
 			}),
